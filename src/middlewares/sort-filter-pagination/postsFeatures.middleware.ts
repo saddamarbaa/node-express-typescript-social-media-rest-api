9bc6641be93a253006106198e78b75979efb10dc
saddamarbaa <saddam@rushowl.sg>
2022-10-15 12:17:29 +0700

    feat: add pagination and sort in api query

--- src/middlewares/sort-filter-pagination/postsFeatures.middleware.ts
+++ src/middlewares/sort-filter-pagination/postsFeatures.middleware.ts
@@ -1,36 +1,37 @@
-// @ts-nocheck
+import { NextFunction } from 'express';
 
-import model from '../../models/Post.model';
+import model from '@src/models/Post.model';
+import { TPaginationRequest, TPaginationResponse } from '@src/interfaces';
 
 export const postsPaginationMiddleware = () => {
-  return async (req, res, next) => {
-    // Pagination
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 1000;
+  return async (req: TPaginationRequest, res: TPaginationResponse, next: NextFunction) => {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 1000;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
 
-    const results = {
+    const results: any = {
       currentPage: {
-        page: page,
-        limit: limit,
+        page,
+        limit,
       },
+      totalDocs: 0,
     };
 
     const totalCount = await model.countDocuments().exec();
-
     results.totalDocs = totalCount;
+
     if (endIndex < totalCount) {
       results.next = {
         page: page + 1,
-        limit: limit,
+        limit,
       };
     }
 
     if (startIndex > 0) {
       results.previous = {
         page: page - 1,
-        limit: limit,
+        limit,
       };
     }
 
@@ -38,43 +39,23 @@ export const postsPaginationMiddleware = () => {
     results.lastPage = Math.ceil(totalCount / limit);
 
     // Sort
-    const sort = {};
-    if (req.query.sortBy && req.query.OrderBy) {
-      sort[req.query.sortBy] = req.query.OrderBy.toLowerCase() === 'desc' ? -1 : 1;
+    const sort: any = {};
+
+    // Sort
+    if (req.query.sortBy && req.query.orderBy) {
+      sort[req.query.sortBy] = req.query.orderBy.toLowerCase() === 'desc' ? -1 : 1;
     } else {
       sort.createdAt = -1;
     }
 
-    // Filter feature is TODO
-    // Filter
-    let filter = {};
-    if (req.query.filterBy && req.query.category) {
-      console.log(req.query.category.toLowerCase());
-      if (req.query.category.toLowerCase() === 'coding') {
-        filter.$or = [{ category: 'Coding' }];
-      } else if (req.query.category.toLowerCase() === 'nodejs') {
-        filter.$or = [{ category: 'Node js' }];
-      } else {
-        filter = {};
-      }
-    }
-
-    // Search
-    if (req.query.search) {
-      filter = {
-        $or: [{ title: { $regex: req.query.title } }, { content: { $regex: req.query.content } }],
-      };
-    }
-    // console.log('filter', filter);
-
     try {
       results.results = await model
-        .find(filter)
+        .find()
         .select('title content postImage author createdAt updatedAt')
-        // .populate(
-        //   'userId',
-        //   'firstName lastName  surname email dateOfBirth gender joinedDate cart isVerified  profileImage  mobileNumber  status role  companyName   acceptTerms nationality  favoriteAnimal  address  profileImage  bio mobileNumber',
-        // )
+        .populate(
+          'author',
+          'name firstName lastName  surname email dateOfBirth gender joinedDate cart isVerified  profileImage  mobileNumber  status role  companyName   acceptTerms nationality  favoriteAnimal  address  profileImage  bio mobileNumber'
+        )
         .limit(limit)
         .sort(sort)
         .skip(startIndex)
