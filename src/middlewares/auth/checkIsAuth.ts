58fb8a25812e00206c5035e5404195360e65bf20
saddamarbaa <saddam@rushowl.sg>
2022-12-06 10:22:06 +0700

    fix: include authenticated user to request body

--- src/middlewares/auth/checkIsAuth.ts
+++ src/middlewares/auth/checkIsAuth.ts
@@ -1,50 +1,45 @@
 import jwt, { VerifyErrors } from 'jsonwebtoken';
 import { NextFunction, Response } from 'express';
+import createHttpError, { InternalServerError } from 'http-errors';
 
-import { response } from '@src/utils';
+import User from '@src/models/User.model';
 import { environmentConfig } from '@src/configs/custom-environment-variables.config';
 import { IAuthRequest, IUser } from '@src/interfaces';
 
-export const isAuth = (req: IAuthRequest, res: Response, next: NextFunction) => {
+export const isAuth = async (req: IAuthRequest, res: Response, next: NextFunction) => {
   const authHeader = (req && req.headers.authorization) || (req && req.headers.Authorization);
   const token = (authHeader && authHeader.split(' ')[1]) || req?.cookies?.authToken || '';
 
   if (!token) {
-    return res.status(401).send(
-      response<null>({
-        data: null,
-        success: false,
-        error: true,
-        message: 'Auth Failed (Invalid Credentials)',
-        status: 401,
-      })
-    );
+    return next(createHttpError(401, 'Auth Failed (Invalid Credentials)'));
   }
 
   jwt.verify(
     token,
     environmentConfig.ACCESS_TOKEN_SECRET_KEY as jwt.Secret,
-    (err: VerifyErrors | null, decodedUser: any) => {
+    async (err: VerifyErrors | null, decodedUser: any) => {
       if (err) {
         // JsonWebTokenError or token has expired
         const errorMessage = err.name === 'JsonWebTokenError' ? 'Auth Failed (Unauthorized)' : err.message;
-        return res.status(403).send(
-          response<null>({
-            data: null,
-            success: false,
-            error: true,
-            message: errorMessage,
-            status: 403,
-          })
-        );
+
+        return next(createHttpError(403, errorMessage));
       }
 
-      // console.log('The Authorized User is ', decodedUser);
-      // Add user to the request
-      req.user = decodedUser as IUser;
+      try {
+        const decodedUserInDB = await User.findOne({ _id: decodedUser?.userId });
 
-      // if we did success go to the next middleware
-      next();
+        if (!decodedUserInDB) {
+          return next(createHttpError(403, `Auth Failed (Unauthorized)`));
+        }
+        // console.log('The Authorized Admin is ', user);
+        // req.user = user as IUser;
+        req.user = decodedUserInDB as IUser;
+
+        // if we did success go to the next middleware
+        next();
+      } catch (error) {
+        return next(InternalServerError);
+      }
     }
   );
 };
